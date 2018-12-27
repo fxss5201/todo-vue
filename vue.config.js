3cb7cf01f052924b3e5ef348ca5f68ea6eb77814
fanyushun <723107234@qq.com>
2018-12-27 14:47:17 +0800

    add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  baseUrl: '/todo-vue/',
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: true,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: {
+    modules: true,
+    sourceMap: true,
+    extract: false
+  }
+}
